313c94d331bd8e6fcf7f40ea81449588369a624c
sanmaoliuco <42509135+sanmaoliuco@users.noreply.github.com>
2022-10-16 15:56:22 +0800

    Update chaoji.py

--- 第五章/chaoji.py
+++ 第五章/chaoji.py
@@ -13,14 +13,14 @@ from chaojiying import Chaojiying_Client
 import time
 
 
-chaojiying = Chaojiying_Client('用户名', '密码', '验证码类型')
+chaojiying = Chaojiying_Client('用户名', '密码', '软件ID')
 
 web = Chrome()
 
 def chj():
     web.get('https://www.chaojiying.com/user/login/')
     img = web.find_element(By.XPATH,'/html/body/div[3]/div/div[3]/div[1]/form/div/img').screenshot_as_png
-    dic = chaojiying.PostPic(img,1902)
+    dic = chaojiying.PostPic(img,1902)   # 1902为验证码类型
     verify_code = dic['pic_str']
 
     web.find_element(By.XPATH,'/html/body/div[3]/div/div[3]/div[1]/form/p[1]/input').send_keys('用户名')
